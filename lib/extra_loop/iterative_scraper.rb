211fd432eb8f2687f649df256c4b29ff1be345b3
Andrea Fiore <and@inventati.org>
2011-12-18 20:34:20 +0000

    Started refactoring iterative scraper
    got rid of #set_extraction_proc (now one can pass the argument for
    building an iteration extractor directly to #set_iteration).

--- lib/extra_loop/iterative_scraper.rb
+++ lib/extra_loop/iterative_scraper.rb
@@ -1,10 +1,6 @@
-require 'extractor'
-require 'scraper_base'
-
 class IterativeScraper < ScraperBase
-  def initialize(urls, arguments = {}, options = {})
-
-    super([], arguments, options)
+  def initialize(urls, options = {}, arguments)
+    super([], options, arguments)
 
     @url_patterns = Array(urls)
     @iteration_set = []
@@ -14,15 +10,25 @@ class IterativeScraper < ScraperBase
     @iteration_param_value = nil
   end
 
-  def set_iteration(param, collection)
-    @iteration_set = Array(collection).collect &:to_s
-    set_iteration_param(param)
-    self
-  end
 
-  def set_iteration_proc(param, selector, closure)
+  # Public
+  #
+  # Sets the scraper iteration by providing either a collection of values (to be interpolated into the URL patterns)
+  # or an extractor block to produce such values.
+  #
+  # param - the name of the parameter to interpolate (NOTE: this is probably not needed).
+  # args  - Either an array of values, or a set the arguments to initialize an Extractor object.
+  #
+  # Returns itself.
+  #
+
+  def set_iteration(param, *args)
+    if args.first.respond_to?(:map)
+      @iteration_set = Array(collection).map &:to_s
+    else
+      @iteration_extractor = Extractor.new(:pagination, *args)
+    end
     set_iteration_param(param)
-    @iteration_extractor = Extractor.new(:pagination, selector, closure)
     self
   end
 
@@ -31,7 +37,7 @@ class IterativeScraper < ScraperBase
       run_iteration(pattern)
 
       while @iteration_set.any?
-        method = @options.async ? :run_iteration_async : :run_iteration
+        method = @options[:async] ? :run_iteration_async : :run_iteration
         self.send(method, pattern)
         @iteration_count += 1
       end
@@ -84,13 +90,19 @@ class IterativeScraper < ScraperBase
     self.class.superclass.instance_method(method).bind(self).call
   end
 
-
   def handle_response(response)
     @iteration_set = extract_iteration_set(response) if @response_count == 0 && @iteration_extractor
     super(response)
   end
 
+  # 
+  # Runs the extractor provided in order to dynamically fetch the array of values needed for the iterative scrape.
+  #
+  # Returns an array of strings.
+  #
+  #
+
   def extract_iteration_set(response)
-    @iteration_extractor.extract_list(response.body).collect(&:to_s)
+    @iteration_extractor.extract_list(response.body).map(&:to_s)
   end
 end
